c7bf47e39ce81346a95a24f8ae59b8d676f56898
Koennn <koenwillemse1@gmail.com>
2017-02-15 20:42:59 +0100

    Initial commit

---
+++ src/main/java/me/koenn/purge/Purge.java
@@ -0,0 +1,16 @@
+package me.koenn.purge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Purge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("All credits for this plugin go to Koenn");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
